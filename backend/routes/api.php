06706e6d47f1242fbe44160b67d4953d846c9511
ImDHAnI769 <bimaramadhani795@gmail.com>
2023-06-02 13:44:14 +0700

    master- bima: membuat fungsi edit,add,view,delete pada folder controler dan model

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Novel;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//route untuk tampil data
+Route::get('/view',[Novel::class,'view']);
+Route::get('/detail/{parameter}',[Novel::class,'detail']);
+// route untuk delete data
+Route::delete('/delete/{parameter}',[Novel::class,'delete']);
+Route::post('/insert',[Novel::class,'insert']);
+Route::put('/update/{parameter}',[Novel::class,'update']);
